6c04170d2b000790eab0e9a2825ac6c513526f74
wmajbour <wmajbour@Hotmail.com>
2025-07-18 09:27:58 -0400

    Added game loop

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import pygame
+from constants import *
+
+
+
 def main():
-    print("Hello from asteroidsremake!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 
+            
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
